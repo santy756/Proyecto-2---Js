90f337e8069b5cfee1443c9aef48614c87218269
lFebanl <124206899+lFebanl@users.noreply.github.com>
2023-05-01 04:15:12 -0300

    admin que crea productos y modifica el locas storage

--- JS/administrar.js
+++ JS/administrar.js
@@ -1,3 +1,6 @@
+//#region lista?
+
+//#endregion
 const productos = JSON.parse(localStorage.getItem("producto_lista"));
 const table = document.getElementById("table");
 const codigo_unico = document.getElementById("codigo_unico");
@@ -8,9 +11,20 @@ const editorial = document.getElementById("editorial");
 const imagen_ilustrativa = document.getElementById("imagen_ilustrativa");
 const descripcion = document.getElementById("descripcion");
 const cantidad_stock = document.getElementById("cantidad_stock");
-const url_error = document.getElementById("url_error");
+const agregar_producto = document.getElementById("agregar_producto");
+const agregar = document.getElementById("agregar_stock");
+const disminuir = document.getElementById("quitar_stock")
 
-console.log(codigo_unico)
+let vcodigo_unico = "";
+let vnombreProd = "";
+let vprecio_unitario = "";
+let vcategoria = "";
+let veditorial = "" ;
+let vimagen_ilustrativa ="";
+let vdescripcion ="";
+let vcantidad_stock ="";
+let vurl_error ="";
+
 
 let contador = 0;
 
@@ -44,9 +58,69 @@ function CreateItem (producto) {
         <td>${producto.precio_unitario}</td>
         <td>${producto.categoria}</td>
         <td>${producto.editorial}</td>
-        <td>${producto.cantidad_stock}</td>
-        <td><button class="btn btn-danger">x</button><td>
+        <td><button class="btn btn-light" id="agregar_stock">+</button> ${producto.cantidad_stock} <button class="btn btn-dark" id="quitar_stock">-</button></td>
+        <td><button class="btn btn-danger" id="${producto.codigo_unico}">x</button><td>
       </tr>`
 }
+codigo_unico.addEventListener("change",function (e) {
+  vcodigo_unico = e.target.value
+})
+nombreProd.addEventListener("change",function (e) {
+  vnombreProd = e.target.value
+})
+precio_unitario.addEventListener("change",function (e) {
+  vprecio_unitario = e.target.value
+})
+categoria.addEventListener("change",function (e) {
+  vcategoria = e.target.value
+})
+editorial.addEventListener("change",function (e) {
+  veditorial = e.target.value
+})
+imagen_ilustrativa.addEventListener("change",function (e) {
+  vimagen_ilustrativa = e.target.value
+})
+descripcion.addEventListener("change",function (e) {
+  vdescripcion = e.target.value
+})
+cantidad_stock.addEventListener("change",function (e) {
+  vcantidad_stock = +e.target.value
+})
 
-console.log(productos)
+agregar_producto.addEventListener("click",function (e) {
+  e.preventDefault()
+  const producto_agregado ={
+    codigo_unico: vcodigo_unico,
+    nombre: vnombreProd,
+    precio_unitario:vprecio_unitario,
+    categoria: vcategoria,
+    editorial: veditorial,
+    imagen_ilustrativa:vimagen_ilustrativa,
+    descripcion:vdescripcion,
+    cantidad_stock: vcantidad_stock,
+    stock_negativo: false,
+    fecha_de_creacion: Date.now(),
+    url_error: "../pages/error404.html",
+  }
+  const productos_agregados = localStorage.getItem("producto_lista");
+  const producto_agregado_objetos =JSON.parse(productos_agregados);
+  producto_agregado_objetos.push(producto_agregado);
+  localStorage.setItem("producto_lista",JSON.stringify(producto_agregado_objetos));
+  window.location.reload();
+  if (vcategoria == "manga"){
+    const mangas_agregados=localStorage.getItem("manga_lista");
+    const manga_agregado_objetos = JSON.parse(mangas_agregados);
+    manga_agregado_objetos.push(producto_agregado);
+    localStorage.setItem("manga_lista",JSON.stringify(manga_agregado_objetos))
+  } else if (vcategoria== "comic"){
+    const comics_agregados=localStorage.getItem("comic_lista");
+    const comic_agregado_objetos = JSON.parse(comics_agregados);
+    comic_agregado_objetos.push(producto_agregado);
+    localStorage.setItem("comic_lista",JSON.stringify(comic_agregado_objetos))
+  }else {
+    const libros_agregados=localStorage.getItem("libros_lista");
+    const libros_agregado_objetos = JSON.parse(libros_agregados);
+    libros_agregado_objetos.push(producto_agregado);
+    localStorage.setItem("libros_lista",JSON.stringify(libros_agregado_objetos))
+  }
+})
